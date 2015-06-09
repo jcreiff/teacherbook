17c97bd894ef449be275b7c78fe84bc04f25dba4
Joe Reiff <jcreiff@gmail.com>
2015-06-09 15:34:24 -0400

    edit login to authenticate bcrypt.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,9 +2,9 @@ class SessionsController < ApplicationController
   def login
     if request.post?
       teacher = Teacher.find_by_email(params[:email])
-      if teacher && teacher.password == params[:password]
+      if teacher && teacher.authenticate(params[:password])
         session[:teacher_id] = teacher.id
-        redirect to parents_path, notice: "Login Successful. Welcome, #{teacher.name[0]}."
+        redirect_to parents_path, notice: "Login Successful. Welcome, #{teacher.name}."
       else
         flash.now[:notice] = "Invalid Login. Please Try Again."
       end
--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -1,5 +1,6 @@
 class Teacher < ActiveRecord::Base
 
+  has_secure_password
   has_many :parents
   validates :name, presence: true
   validates :email, presence: true, uniqueness: true
