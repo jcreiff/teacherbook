486a61b1d833553cd9a820bcf9d807fba8b1abbb
Joe Reiff <jcreiff@gmail.com>
2015-06-09 17:29:29 -0400

    add logged_in? to teacher controller actions

--- app/controllers/teachers_controller.rb
+++ app/controllers/teachers_controller.rb
@@ -1,4 +1,6 @@
 class TeachersController < ApplicationController
+  before_action :logged_in?
+
   def home
   end
 
@@ -7,4 +9,11 @@ class TeachersController < ApplicationController
 
   def update
   end
+
+  private
+
+    def logged_in?
+      Teacher.find_by_id(session[:teacher_id]) ? true : (redirect_to sessions_login_path, notice: "Please Login.")
+    end
+
 end
