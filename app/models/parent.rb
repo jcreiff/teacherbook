67d24ba57bd9b89149d9e0952a1b541efb1e3bea
Joe Reiff <jcreiff@gmail.com>
2015-06-09 14:51:51 -0400

    add validations, faker

--- app/models/parent.rb
+++ app/models/parent.rb
@@ -1,5 +1,9 @@
 class Parent < ActiveRecord::Base
 
   belongs_to :teacher
+  validates :teacher_id, presence: true
+  validates :name, presence: true
+  validates :student_name, presence: true
+  validates :email, presence: true
 
 end
--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -1,5 +1,8 @@
 class Teacher < ActiveRecord::Base
 
   has_many :parents
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates :password_digest, presence: true
 
 end
