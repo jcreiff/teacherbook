d51c5078577985890b6127a9e195f621e14881f6
Joe Reiff <jcreiff@gmail.com>
2015-06-09 17:25:34 -0400

    create Teachers controller, set root to teachers/home

---
+++ app/controllers/teachers_controller.rb
@@ -0,0 +1,10 @@
+class TeachersController < ApplicationController
+  def home
+  end
+
+  def edit
+  end
+
+  def update
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  root 'teachers#home'
+
+  get 'teachers/home'
+  get 'teachers/edit'
+  post 'teachers/update'
+
   get 'sessions/login'
   post 'sessions/login'
   get 'sessions/logout'
