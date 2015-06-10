c7400f41c8ad4726c4bac9f06c1077f330e0421a
Joe Reiff <jcreiff@gmail.com>
2015-06-09 23:47:25 -0400

    add seeds for teachers & parents. #shipit

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Teacher.create!(name: "Seedy McSeeds", email: "seedy@seeds.seed", password: "seeds")
+
+4.times do
+  Teacher.create!(name: Faker::Name.name, email: Faker::Internet.email, password: "seeds")
+end
+
+ids = Teacher.all.map {|t| t.id}
+
+50.times do
+  Parent.create!(name: Faker::Name.name, email: Faker::Internet.email,
+      student_name: Faker::Name.name, teacher_id: ids.sample)
+end
